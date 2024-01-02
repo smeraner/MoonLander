f062da62bb6a89a6ad9f6d4bd31be77a42b8b382
Simon Meraner <simon.meraner@gmail.com>
2024-01-03 00:20:21 +0100

    Add new files and update existing files

---
+++ src/app.ts
@@ -0,0 +1,399 @@
+import * as THREE from 'three';
+import * as Tween from 'three/examples/jsm/libs/tween.module.js';
+import Stats from 'three/addons/libs/stats.module.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { createText } from 'three/addons/webxr/Text2D.js';
+
+import { Player } from './player';
+import { World } from './world';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+export class App {
+    static firstUserActionEvents = ['mousedown', 'touchstart', /*'mousemove','scroll',*/'keydown','gamepadconnected'];
+    static firstUserAction = true;
+    static gui: GUI = new GUI({ width: 200 });
+
+    private player: Player | undefined;
+    private renderer: THREE.WebGLRenderer;
+    private instructionText: any;
+    private world: World | undefined;
+    private GRAVITY: number = 9.8;
+
+    private keyStates: any = {};
+    private clock: any;
+    private STEPS_PER_FRAME = 5;
+    private stats: Stats = new Stats();
+    private scene: THREE.Scene | undefined;
+
+    private audioListenerPromise: Promise<THREE.AudioListener>;
+    private container: HTMLDivElement;
+    public setAudioListener: any;
+    private camera: THREE.PerspectiveCamera | undefined;
+    private filterMesh: THREE.Mesh<THREE.SphereGeometry, THREE.MeshBasicMaterial, THREE.Object3DEventMap> | undefined;
+    private orbitVontrols: OrbitControls | undefined;
+    private gamepad: Gamepad | null | undefined;
+    private touchStartX= 0;
+    private touchStartY= 0;
+    private touchMoveX= 0;
+    private touchMoveY= 0;
+    private deferredInstallPrompt: any;
+
+    constructor() {
+        this.clock = new THREE.Clock();
+        //App.gui.hide();
+        this.initDebugGui();
+
+        this.container = document.createElement('div');
+        document.body.appendChild(this.container);
+
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: window.devicePixelRatio <= 1,
+            powerPreference: "high-performance"
+        });
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.VSMShadowMap;
+        this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
+        this.container.appendChild(this.renderer.domElement);
+
+        this.container.appendChild( this.stats.dom );
+
+        this.audioListenerPromise = new Promise<THREE.AudioListener>((resolve) => {
+            this.setAudioListener = resolve;
+        });
+
+        this.init();
+    }
+
+    async init() {
+       
+        await this.initScene();
+
+        App.firstUserActionEvents.forEach((event) => {
+            document.addEventListener(event, this.onFirstUserAction.bind(this), { once: true });
+        });
+
+        window.addEventListener('beforeinstallprompt', (e) => {
+            console.log('beforeinstallprompt Event fired');
+            e.preventDefault();          
+            // Stash the event so it can be triggered later.
+            this.deferredInstallPrompt = e;
+            
+            return false;
+          });
+
+        window.addEventListener('resize', this.resize.bind(this));
+        document.addEventListener('keydown', (event) => this.keyStates[event.code] = true);
+        document.addEventListener('keyup', (event) => this.keyStates[event.code] = false);
+
+        window.addEventListener("touchmove", (e) => this.hanldeTouch(e));
+        window.addEventListener("touchstart", (e) => this.hanldeTouch(e));
+        window.addEventListener("touchend", (e) => this.hanldeTouch(e));
+
+        window.addEventListener('mousedown', () => this.renderer.domElement.requestPointerLock());
+        window.addEventListener('mousemove', (e) => {
+            if(!this.player) return;
+            this.player.rotation.y -= e.movementX * 0.001;
+            this.player.rotation.x += e.movementY * 0.001;
+        });
+        window.addEventListener("gamepadconnected", (e) => {
+            this.gamepad = e.gamepad;
+            console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+            this.gamepad.index, this.gamepad.id,
+            this.gamepad.buttons.length, this.gamepad.axes.length);
+        });
+
+        this.renderer.setAnimationLoop(this.update.bind(this));
+    }
+
+    hanldeTouch(e: TouchEvent) {
+        var touch = e.touches[0] || e.changedTouches[0];
+        if(!touch) return;
+        const x = touch.pageX;
+        const y = touch.pageY;
+
+        if(e.type === "touchstart") {
+            this.touchStartX = x;
+            this.touchStartY = y;
+            this.touchMoveX = 0;
+            this.touchMoveY = 0;
+        } else if(e.type === "touchend") {
+            this.touchMoveX = 0;
+            this.touchMoveY = 0;
+        } else if(e.type === "touchmove") {
+            this.touchMoveX = 4*(x - this.touchStartX)/window.innerWidth;
+            this.touchMoveY = 4*(y - this.touchStartY)/window.innerHeight;
+        }
+    }
+
+    initDebugGui() {
+        App.gui.add({ debugPlayer: false }, 'debugPlayer')
+            .onChange(function (value) {
+                Player.debug = value;
+            });
+        App.gui.add({ debugWorld: false }, 'debugWorld')
+            .onChange((value: boolean) => {
+                if (this.world && this.world.helper) {
+                    this.world.helper.visible = value;
+                }
+            });
+    }
+
+
+    /**
+     * Executes actions when the user performs their first interaction.
+     * Plays audio and adds a light saber to the player's scene.
+     */
+    onFirstUserAction() {
+        if(App.firstUserAction === false) return;
+        App.firstUserAction = false;
+
+        App.firstUserActionEvents.forEach((event) => {
+            document.removeEventListener(event, this.onFirstUserAction.bind(this));
+        });
+
+        this.askInstallPWA();
+
+        document.getElementById('loading')?.remove();
+
+        //init audio
+        const listener = new THREE.AudioListener();
+        if (this.setAudioListener) {
+            this.setAudioListener(listener);
+        }
+
+        window.addEventListener('blur', () => listener.context.suspend());
+        window.addEventListener('focus', () => listener.context.resume());
+    }
+
+    askInstallPWA() {
+        //already pwa
+        if (window.matchMedia('(display-mode: standalone)').matches || (window.navigator as any)["standalone"] === true) {
+            return;
+        }
+
+        if(this.deferredInstallPrompt) this.deferredInstallPrompt.prompt();
+    }
+
+    /***
+     * @returns {Promise}
+     */
+    async initScene() {
+
+        //init world
+        this.world = new World(this.audioListenerPromise, App.gui);
+        this.world.addEventListener('needHudUpdate', () => this.updateHud());
+        this.world.addEventListener('collect', () => {
+            this.vibrate(100);
+        });
+        this.scene = await this.world.loadScene();
+
+        let fov = 70;
+        this.camera = new THREE.PerspectiveCamera(
+            fov,
+            window.innerWidth / window.innerHeight,
+        )
+
+        let filterGeometry = new THREE.SphereGeometry(0.5, 15, 32); // camera near is 0.1, camera goes inside this sphere
+        let filterMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, transparent: true, opacity: 0.35, side: THREE.BackSide});
+        let filterMesh = new THREE.Mesh(filterGeometry, filterMaterial);
+        filterMesh.visible = false;
+        this.camera.add(filterMesh);
+        this.filterMesh = filterMesh;
+
+        //init player
+        this.player = new Player(this.scene, this.camera, this.GRAVITY);
+        this.player.teleport(this.world.playerSpawnPoint);
+        this.player.addEventListener('dead', () => {
+            this.vibrate(1000);
+            this.updateHud();
+            if(!this.world || !this.player) return;
+            this.player.teleport(this.world.playerSpawnPoint);
+            this.world.allLightsOff();
+            this.world.stopWorldAudio();
+
+            setTimeout(() => {
+                this.restart();
+            }, 3000);
+        });
+        this.player.addEventListener('damaged', () => {
+            this.vibrate(100);
+            this.blendHit();
+            this.updateHud();
+        });
+        this.scene.add(this.player);
+        this.updateHud();
+
+        //this.enableOrbitControls();
+
+        this.resize();
+    }
+
+    enableOrbitControls() {
+        if(!this.camera || !this.renderer) return;
+        this.orbitVontrols = new OrbitControls( this.camera, this.renderer.domElement );
+    }
+
+    restart() {
+        if(!this.world || !this.player) return;
+        this.player.teleport(this.world.playerSpawnPoint);
+        this.player.reset();
+        this.world.reset();
+        this.updateHud();
+    }
+
+    vibrate(ms = 100) {
+        if(navigator.vibrate) navigator.vibrate(ms);
+        if(this.gamepad && this.gamepad.vibrationActuator) {
+            this.gamepad.vibrationActuator.playEffect("dual-rumble", {
+                startDelay: 0,
+                duration: ms,
+                weakMagnitude: 1,
+                strongMagnitude: 1
+            });
+        } 
+    }
+
+    blendHit() {
+        if(!this.filterMesh) return;
+        this.filterMesh.material.color.setHex(0xff0000);
+        this.filterMesh.material.opacity = 0.35;
+        this.filterMesh.visible = true;
+        setTimeout(() => {
+            if(!this.filterMesh) return;
+            this.filterMesh.visible = false;
+        }, 200);
+    }
+
+    blendDie() {
+        if(!this.filterMesh) return;
+        this.filterMesh.material.color.setHex(0xff0000);
+        this.filterMesh.material.opacity = 1;
+        this.filterMesh.visible = true;
+    }
+
+    blendBlack() {
+        if(!this.filterMesh) return;
+        this.filterMesh.material.color.setHex(0x000000);
+        this.filterMesh.material.opacity = 1;
+        this.filterMesh.visible = true;
+    }
+
+    blendClear() {
+        if(!this.filterMesh) return;
+        this.filterMesh.visible = false;
+    }
+
+    displayWinMessage() {
+        if(!this.player || !this.world) return;
+        this.blendBlack();
+        this.updateInstructionText("You win! Reload to restart.");
+        this.world.allLightsOff();
+        this.world.stopWorldAudio();
+    }
+
+    private resize(): void {
+        if(!this.player || !this.camera) return;
+
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    }
+
+    private controls(deltaTime: number): void {
+        if(!this.player) return;       
+        const speedDelta = deltaTime * (this.player.onFloor ? this.player.speedOnFloor : this.player.speedInAir);
+
+        //keyboard controls
+        if (this.keyStates['KeyW']) {
+            this.player.velocity.add(this.player.getForwardVector().multiplyScalar(speedDelta));
+        }
+
+        if (this.keyStates['KeyS']) {
+            this.player.velocity.add(this.player.getForwardVector().multiplyScalar(-speedDelta));
+        }
+
+        if (this.keyStates['KeyA']) {
+            this.player.velocity.add(this.player.getSideVector().multiplyScalar(-speedDelta));
+        }
+
+        if (this.keyStates['KeyD']) {
+            this.player.velocity.add(this.player.getSideVector().multiplyScalar(speedDelta));
+        }
+
+        if (this.keyStates['Space']) {
+            this.player.jump();
+        }
+
+        //touch move
+        if(this.touchMoveX > 0 || this.touchMoveY > 0) {
+            this.player.velocity.add(this.player.getSideVector().multiplyScalar(this.touchMoveX * speedDelta));
+            this.player.velocity.add(this.player.getForwardVector().multiplyScalar(-this.touchMoveY * speedDelta));
+        }
+
+        //gamepad controls
+        if(this.gamepad) {
+            this.gamepad = navigator.getGamepads()[this.gamepad.index];
+            if(!this.gamepad) return;
+            this.player.velocity.add(this.player.getForwardVector().multiplyScalar(-this.gamepad.axes[1] * speedDelta));
+            this.player.velocity.add(this.player.getSideVector().multiplyScalar(this.gamepad.axes[0] * speedDelta));
+            if(this.gamepad.buttons[0].pressed) {
+                this.player.jump();
+            }
+        }
+
+    }
+
+    private updateHud(){
+        if(!this.player) return;
+
+        let hudText = `L ${this.world?.getLevel()} `;
+        if(this.player.health === 0) {
+            hudText = " ☠ Game over. Wait to restart.";
+        } else {
+            hudText += ` ♥ ${this.player.health.toFixed(0)}`;
+        }
+        hudText += ` 🞖 ${this.world?.metersToLanding} m`;
+
+        this.updateInstructionText(hudText);
+    }
+
+    private updateInstructionText(text: string): void {
+        if(!this.player || !this.camera) return;
+
+        this.camera.remove(this.instructionText);
+        this.instructionText = createText(text, 0.04);
+        this.instructionText.position.set(0,0.1,-0.2);
+        this.instructionText.scale.set(0.3,0.3,0.3);
+        this.camera.add(this.instructionText);
+    }
+
+    private teleportPlayerIfOob(): void {
+        if(!this.player || !this.world) return;
+        if (this.world && this.player.position.y <= -25) {
+            this.player.teleport(this.world.playerSpawnPoint);
+        }
+    }
+
+    public update(): void {
+        if(!this.player || !this.scene || !this.world || !this.camera) return;
+
+        const deltaTime = Math.min(0.05, this.clock.getDelta()) / this.STEPS_PER_FRAME;
+
+        for (let i = 0; i < this.STEPS_PER_FRAME; i++) {
+            this.controls(deltaTime);
+
+            this.player.update(deltaTime, this.world);
+            this.world.update(deltaTime, this.player);
+
+            this.teleportPlayerIfOob();
+        }
+
+        this.orbitVontrols?.update(deltaTime);
+        Tween.update(deltaTime);
+        this.stats.update();
+        this.renderer.render(this.scene, this.camera);
+    }
+}
---
+++ src/world.ts
@@ -0,0 +1,298 @@
+import * as THREE from 'three';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { Lensflare, LensflareElement } from 'three/addons/objects/Lensflare.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import { OctreeHelper } from 'three/addons/helpers/OctreeHelper.js';
+import { Octree } from 'three/addons/math/Octree.js';
+import { Player } from './player';
+
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('./draco/');
+const geometryLoader = new GLTFLoader();
+geometryLoader.setDRACOLoader(dracoLoader);
+
+interface WorldEventMap extends THREE.Object3DEventMap {
+    needHudUpdate: WorldNeedHudUpdateEvent;
+    levelUp: WorldLevelUpEvent;
+}
+
+export interface WorldNeedHudUpdateEvent extends THREE.Event {
+    type: 'needHudUpdate';
+}
+
+export interface WorldLevelUpEvent extends THREE.Event {
+    type: 'levelUp';
+}
+
+type Weather = 'rain' | 'snow' | 'none';
+
+export class World extends THREE.Object3D<WorldEventMap> {
+
+    static debug = false;
+    static soundBufferAmbient: Promise<AudioBuffer>;
+    static model: Promise<THREE.Object3D>;
+    static treeModel: Promise<THREE.Object3D>;   
+    static initialize() {
+        //load audio     
+        const audioLoader = new THREE.AudioLoader();
+        World.soundBufferAmbient = audioLoader.loadAsync('./sounds/ambient.mp3');
+
+        //load model
+        const gLTFLoader = new GLTFLoader();
+        World.treeModel = gLTFLoader.loadAsync('./models/tree.glb').then(gltf => {
+            gltf.scene.scale.set(0.002, 0.002, 0.002);
+            return gltf.scene;
+        });
+        // World.soundBufferIntro = audioLoader.loadAsync('./sounds/intro.ogg');
+    }
+
+    worldOctree = new Octree();
+
+    gui: GUI;
+    playerSpawnPoint: THREE.Vector3;
+    scene: THREE.Scene | undefined;
+    soundAmbient: THREE.Audio | undefined;
+    soundIntro: THREE.Audio | undefined;
+    map: THREE.Object3D<THREE.Object3DEventMap> | undefined;
+    helper: OctreeHelper | undefined;
+    animatedObjects: THREE.Object3D[] = [];
+    private moon: THREE.Mesh | undefined;
+    metersToLanding: number = 0;
+
+    /**
+     * @param {Promise<THREE.AudioListener>} audioListenerPromise
+     * @param {GUI} gui
+     */
+    constructor(audioListenerPromise: Promise<THREE.AudioListener>, gui: GUI) {
+        super();
+
+        this.gui = gui;
+        this.playerSpawnPoint = new THREE.Vector3(0, 3, -200);
+
+        setInterval(() => {
+            this.dispatchEvent({ type: 'needHudUpdate' } as WorldNeedHudUpdateEvent);
+        }, 200);
+
+        this.initAudio(audioListenerPromise);
+    }
+
+    async initAudio(audioListenerPromise: Promise<THREE.AudioListener>) {
+        const audioListener = await audioListenerPromise;
+        const soundBufferAmbient = await World.soundBufferAmbient;
+        this.soundAmbient = new THREE.Audio(audioListener);
+        this.soundAmbient.setBuffer(soundBufferAmbient);
+        this.soundAmbient.setLoop(true);
+        this.soundAmbient.setVolume(1);
+
+        this.playWorldAudio();
+    }
+
+    playWorldAudio() {
+        if (this.soundAmbient && !this.soundAmbient.isPlaying) {
+            this.soundAmbient.play();
+        }
+        setTimeout(() => {
+            if (!this.soundIntro || this.soundIntro.isPlaying) return;
+            this.soundIntro.play();
+        }, 1000);
+    }
+
+    stopWorldAudio() {
+        if (this.soundAmbient) {
+            this.soundAmbient.stop();
+        }
+    }
+
+    async loadScene(): Promise<THREE.Scene> {
+        this.scene = new THREE.Scene();
+
+        //load texture
+        const textureLoader = new THREE.TextureLoader();
+        const moonTexture = await textureLoader.loadAsync('./textures/moon.jpg');
+        const moonNormalTexture = await textureLoader.loadAsync('./textures/moon_normal.jpg');
+
+
+        //big world roller geometry
+        const map = this.map = new THREE.Object3D();
+        // const widthHeight = 1024;
+        // const dataNoiseTexture = new THREE.DataTexture(new Uint8Array(widthHeight * widthHeight * 4), widthHeight, widthHeight, THREE.RGBAFormat);
+        // dataNoiseTexture.wrapS = THREE.RepeatWrapping;
+        // dataNoiseTexture.wrapT = THREE.RepeatWrapping;
+        // dataNoiseTexture.repeat.set(5, 2);
+        // dataNoiseTexture.needsUpdate = true;
+
+        // for (let i = 0; i < dataNoiseTexture.image.data.length; i += 4) {
+        //     //random number between 200 and 255
+        //     const x = Math.floor(Math.random() * 55) + 200;
+        //     dataNoiseTexture.image.data[i + 0] = x;
+        //     dataNoiseTexture.image.data[i + 1] = x;
+        //     dataNoiseTexture.image.data[i + 2] = x;
+        //     dataNoiseTexture.image.data[i + 3] = x;
+        // }
+        const moonMaterial = new THREE.MeshStandardMaterial({ 
+            color: 0xffffff, 
+            map: moonTexture, 
+            displacementMap: moonTexture, 
+            displacementScale: 0.2,
+            normalMap: moonNormalTexture,
+            normalScale: new THREE.Vector2(0.3, 0.3)
+         });
+
+        const moonGeometry = new THREE.SphereGeometry(17, 64, 64); //1.737,4 km
+        this.moon = new THREE.Mesh(moonGeometry, moonMaterial);
+        map.add(this.moon);
+
+        this.rebuildOctree();
+
+        this.scene.add(map);
+
+        this.addHemisphere();
+
+        const helper = new OctreeHelper(this.worldOctree);
+        helper.visible = false;
+        this.scene.add(helper);
+        this.helper = helper;
+
+        return this.scene;
+    }
+
+    reset() {
+        this.stopWorldAudio();
+        this.playWorldAudio();
+        this.allLightsOn();
+    }
+
+    allLightsOff() {
+        if (!this.scene) return;
+
+        this.scene.traverse(child => {
+            if ((child as THREE.Light).isLight) {
+                child.visible = false;
+            }
+        });
+    }
+
+    allLightsOn() {
+        if (!this.scene) return;
+
+        this.scene.traverse(child => {
+            if ((child as THREE.Light).isLight) {
+                child.visible = true;
+            }
+        });
+    }
+
+    addFog() {
+        if (!this.scene) return;
+
+        this.scene.fog = new THREE.Fog(0xffffff, 10, 35);
+    }
+
+    getLevel() {
+        return 1;
+    }
+
+    async addHemisphere() {
+        if (!this.scene) return;
+
+        //check if scene has hemisphere
+        let hemisphere = this.scene.getObjectByName("Hemisphere");
+        if (hemisphere) hemisphere.removeFromParent();
+
+        hemisphere = new THREE.Group();
+        hemisphere.name = "Hemisphere";
+
+        // Sky
+        this.scene.background = new THREE.Color(0x000000);
+
+        const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+        hemisphereLight.position.set(0, 10, 0);
+        hemisphereLight.intensity = .1;
+        hemisphere.add(hemisphereLight);
+
+        const directionalLight = new THREE.DirectionalLight(0xffffff)//toGradient);
+        directionalLight.position.set(-1000, 20, -10);
+        directionalLight.rotation.set(-Math.PI/2, 0, 0);
+        directionalLight.castShadow = true;
+        hemisphere.add(directionalLight);
+
+        //lensflare
+        const textureLoader = new THREE.TextureLoader();
+        const textureFlare0 = await textureLoader.loadAsync('./textures/lensflare0.png');
+        const textureFlare3 = await textureLoader.loadAsync('./textures/lensflare3.png');
+
+        const lensflare = new Lensflare();
+        lensflare.addElement(new LensflareElement(textureFlare0, 700, 0, directionalLight.color));
+        lensflare.addElement(new LensflareElement(textureFlare3, 70, 0.7));
+
+        directionalLight.add(lensflare);
+
+
+        const particleCount = 15000;
+        const particleColor = 0xffffff;
+        const particleSize = 0.1;
+        const particlesAreaSize = [1000, 1000, 1000];
+        const particleFreeAreaSize = [500, 500, 500];
+        const positions = [];
+        const sizes = [];
+        const particlesGeo = new THREE.BufferGeometry();
+        for (let i = 0; i < particleCount; i++) {
+            const x = Math.random() * particlesAreaSize[0] - particlesAreaSize[0] / 2;
+            const y = Math.random() * particlesAreaSize[1] - particlesAreaSize[1] / 2;
+            const z = Math.random() * particlesAreaSize[2] - particlesAreaSize[2] / 2;
+            if (x > -particleFreeAreaSize[0] / 2 && x < particleFreeAreaSize[0] / 2 &&
+                y > -particleFreeAreaSize[1] / 2 && y < particleFreeAreaSize[1] / 2 &&
+                z > -particleFreeAreaSize[2] / 2 && z < particleFreeAreaSize[2] / 2) {
+                continue;
+            }
+            positions.push(x, y, z);
+            sizes.push(particleSize);
+        }
+        particlesGeo.setAttribute("position", new THREE.BufferAttribute(new Float32Array(positions), 3));
+        particlesGeo.setAttribute("size", new THREE.BufferAttribute(new Float32Array(sizes), 1));
+
+        const rainMaterial = new THREE.PointsMaterial({
+            color: particleColor,
+            size: particleSize,
+            //transparent: true
+        });
+        const particles = new THREE.Points(particlesGeo, rainMaterial);
+
+        this.scene.add(particles);
+
+        this.scene.add(hemisphere);
+    }
+
+
+    update(deltaTime: number, player: Player) {
+        if (!this.moon) return;
+
+        this.moon.rotation.y += 0.05 * deltaTime;
+
+        this.metersToLanding = Number(((player.position.distanceTo(this.moon.position)-17) * 100).toFixed(2));
+        // this.animatedObjects.forEach(object => {
+        // });
+        // gravity player to moon 1,62 m/s²
+        const moonGlobalPosition = new THREE.Vector3();
+        this.moon.getWorldPosition(moonGlobalPosition);
+        player.velocity.addScaledVector(moonGlobalPosition.sub(player.position).normalize(), 0.162 * deltaTime);
+
+        //check if player is near placeholder
+        this.checkPlayerCollision(player);
+
+    }
+
+    private checkPlayerCollision(player: Player) {
+        const playerGlobalPosition = new THREE.Vector3();
+        player.getWorldPosition(playerGlobalPosition);
+
+    }
+
+    rebuildOctree() {
+        if (this.map) {
+            this.worldOctree.clear().fromGraphNode(this.map);
+        }
+    }
+}
+World.initialize();
