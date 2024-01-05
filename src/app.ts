b5e8d7b17d7467d87e77a5377dcdd86810b47375
Simon Meraner <simon.meraner@gmail.com>
2024-01-05 12:52:19 +0100

    Add fadeClear method to App class

--- src/app.ts
+++ src/app.ts
@@ -180,6 +180,7 @@ export class App {
         
         //start game loop
         this.renderer.setAnimationLoop(this.update.bind(this));
+        this.fadeClear(2000, 0xffffff);
     }
 
     askInstallPWA() {
@@ -255,64 +256,56 @@ export class App {
         this.bloomComposer.addPass( bloomPass );*/
 
         const renderScene = new RenderPass( this.scene, this.camera );
-        // const mixPass = new ShaderPass(LuminosityShader);
-        //     new THREE.ShaderMaterial( {
-        //         uniforms: {
-        //             baseTexture: { value: null },
-        //             bloomTexture: { value: this.bloomComposer.renderTarget2.texture }
-        //         },
-        //         vertexShader: `
-        //             varying vec2 vUv;
-        //             void main() {
-        //                 vUv = uv;
-        //                 gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-        //             }`,
-        //         fragmentShader: `
-        //             uniform sampler2D baseTexture;
-        //             uniform sampler2D bloomTexture;
+        const mixPass = new ShaderPass(
+            new THREE.ShaderMaterial( {
+                uniforms: {
+                    baseTexture: { value: null },
+                },
+                vertexShader: `
+                    varying vec2 vUv;
+                    void main() {
+                        vUv = uv;
+                        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+                    }`,
+                fragmentShader: /* glsl */`
+                    uniform sampler2D baseTexture;
+                    varying vec2 vUv;
         
-        //             varying vec2 vUv;
-        
-        //             void main() {
-        
-        //                 gl_FragColor = ( texture2D( baseTexture, vUv ) + vec4( 1.0 ) * texture2D( bloomTexture, vUv ) );
-        
-        //             }
-        //             float warp = 0.75; // simulate curvature of CRT monitor
-        //             float scan = 0.75; // simulate darkness between scanlines
+                    float warp = 0.75; // simulate curvature of CRT monitor
+                    float scan = 0.75; // simulate darkness between scanlines
                     
-        //             void mainImage(out vec4 fragColor,in vec2 fragCoord)
-        //             {
-        //             // squared distance from center
-        //             vec2 uv = fragCoord/iResolution.xy;
-        //             vec2 dc = abs(0.5-uv);
-        //             dc *= dc;
+                    void main()
+                    {
+                        // squared distance from center
+                        vec2 uv = vUv;
+                        vec2 dc = abs(0.5-uv);
+                        dc *= dc;
+                        
+                        // warp the fragment coordinates
+                        uv.x -= 0.5; uv.x *= 1.0+(dc.y*(0.3*warp)); uv.x += 0.5;
+                        uv.y -= 0.5; uv.y *= 1.0+(dc.x*(0.4*warp)); uv.y += 0.5;
                     
-        //             // warp the fragment coordinates
-        //             uv.x -= 0.5; uv.x *= 1.0+(dc.y*(0.3*warp)); uv.x += 0.5;
-        //             uv.y -= 0.5; uv.y *= 1.0+(dc.x*(0.4*warp)); uv.y += 0.5;
-                
-        //             // sample inside boundaries, otherwise set to black
-        //             if (uv.y > 1.0 || uv.x < 0.0 || uv.x > 1.0 || uv.y < 0.0)
-        //                 fragColor = vec4(0.0,0.0,0.0,1.0);
-        //             else
-        //                 {
-        //                 // determine if we are drawing in a scanline
-        //                 float apply = abs(sin(fragCoord.y)*0.5*scan);
-        //                 // sample the texture
-        //                 fragColor = vec4(mix(texture(iChannel0,uv).rgb,vec3(0.0),apply),1.0);
-        //                 }
-        //             }`,
-        //         defines: {}
-        //     } ), 'baseTexture'
-        // );
-        // mixPass.needsSwap = true;
+                        // sample inside boundaries, otherwise set to black
+                        if (uv.y > 1.0 || uv.x < 0.0 || uv.x > 1.0 || uv.y < 0.0)
+                            gl_FragColor = vec4(0.0,0.0,0.0,1.0);
+                        else
+                            {
+                            // determine if we are drawing in a scanline
+                            float apply = abs(sin(gl_FragCoord.y)*0.5*scan);
+                            // sample the texture
+                            gl_FragColor = vec4(mix(texture(baseTexture,uv).rgb,vec3(0.0),apply),1.0);
+                            }
+                    }`,
+            }), 'baseTexture'
+        );
+        mixPass.needsSwap = true;
 
         const outputPass = new OutputPass();
 
-        // this.finalComposer = new EffectComposer( this.renderer );
-        // this.finalComposer.addPass( renderScene );
-        // this.finalComposer.addPass( outputPass );
+        this.finalComposer = new EffectComposer( this.renderer );
+        this.finalComposer.addPass( renderScene );
+        this.finalComposer.addPass( mixPass );
+        this.finalComposer.addPass( outputPass );
 
         //this.enableOrbitControls();
 
@@ -356,8 +349,8 @@ export class App {
         this.fade(0x000000, 1, ms);
     }
 
-    fadeClear(ms = 1000) {
-        this.fade(0x00000000, 0, ms);
+    fadeClear(ms = 1000, from = 0x00000000) {
+        this.fade(from, 0, ms);
     }
 
     fade(color = 0x000000, direction = 1, ms = 1000) {
